a27619134f95ff1275e616b5279057d08d3cc82d
muhsinfaris <muhsinfariskalodi@gmail.com>
2023-09-21 11:58:28 +0530

    initial comit

---
+++ bin/operators.dart
@@ -0,0 +1,20 @@
+void main()
+{
+  //arithmetic operator + - / % ~/
+
+  int a=200,b=6;
+  print("$a+$b = ${a+b}");
+  print("$a-$b = ${a-b}");
+  print("$a/$b = ${a/b}");
+  print("$a%$b = ${a%b}");
+  print("$a~/$b = ${a~/b}");
+
+  // assignment operastors = += -= *= /= %= ~/= etc
+  dynamic x=12,y=5;
+  print("x=y -> ${x=y}");
+  print("x+=y -> ${x+=y}");
+  print("x-=y -> ${x-=y}");
+  print("x*=y -> ${x*=y}");
+  print("x/=y -> ${x/=y}");
+  print("x~/=y -> ${x~/=y}");
+}
